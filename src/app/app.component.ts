e84294fed8577838839f985494521f33228518c6
chicagoairmailcc <chicagoairmail@protonmail.com>
2019-08-20 15:50:40 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements AfterContentChecked {
   name = 'Angular';
+  ngAfterContentChecked() {
+    console.log(
+      this
+    );
+  }
 }
